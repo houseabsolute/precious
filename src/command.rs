51c6b05eeefae04ba36a1d80c85646785df33209
Dave Rolsky <autarch@urth.org>
2019-04-20 11:57:38 -0500

    Refactor command submodule innards

--- src/command.rs
+++ src/command.rs
@@ -1,14 +1,21 @@
 use failure::Error;
 use log::debug;
-use std::collections::HashSet;
 #[cfg(target_family = "unix")]
 use std::os::unix::prelude::*;
-use std::process::{Command, ExitStatus};
+use std::path::PathBuf;
+use std::process;
 
 #[derive(Debug, Fail)]
 pub enum CommandError {
-    #[fail(display = "Got unexpected exit code {} from `{}`", code, cmd)]
-    ExitCodeIsNotZero { cmd: String, code: i32 },
+    #[fail(
+        display = "Got unexpected exit code {} from `{}. Stderr was {}`",
+        code, cmd, stderr
+    )]
+    ExitCodeIsNotZero {
+        cmd: String,
+        code: i32,
+        stderr: String,
+    },
 
     #[fail(display = "Ran `{}` and it was killed by a signal: {}", cmd, signal)]
     ProcessKilledBySignal { cmd: String, signal: i32 },
@@ -27,21 +34,56 @@ pub struct CommandResult {
 pub fn run_command(
     cmd: String,
     args: Vec<String>,
-    ok_exit_codes: &HashSet<i32>,
+    ok_exit_codes: Vec<i32>,
     expect_stderr: bool,
+    in_dir: Option<&PathBuf>,
 ) -> Result<CommandResult, Error> {
-    let mut c = Command::new(cmd.clone());
+    let mut c = process::Command::new(cmd.clone());
     for a in args.iter() {
         c.arg(a);
     }
+    if in_dir.is_some() {
+        c.current_dir(in_dir.unwrap());
+    }
 
+    let output = output_from_command(c, ok_exit_codes, &cmd, &args)?;
+
+    if !output.stderr.is_empty() && !expect_stderr {
+        return Err(CommandError::UnexpectedStderr {
+            cmd: command_string(&cmd, &args),
+            stderr: String::from_utf8(output.stderr)?,
+        })?;
+    }
+
+    let code = match output.status.code() {
+        Some(code) => code,
+        None => -1,
+    };
+
+    Ok(CommandResult {
+        exit_code: code,
+        stdout: to_option_string(output.stdout),
+        stderr: to_option_string(output.stderr),
+    })
+}
+
+fn output_from_command(
+    mut c: process::Command,
+    ok_exit_codes: Vec<i32>,
+    cmd: &String,
+    args: &Vec<String>,
+) -> Result<process::Output, Error> {
     let output = c.output()?;
     match output.status.code() {
         Some(code) => {
             if !ok_exit_codes.contains(&code) {
                 let cstr = command_string(cmd, args);
                 debug!("Ran {} and got exit code of {}", cstr.clone(), code);
-                return Err(CommandError::ExitCodeIsNotZero { cmd: cstr, code })?;
+                return Err(CommandError::ExitCodeIsNotZero {
+                    cmd: cstr,
+                    code,
+                    stderr: String::from_utf8(output.stderr)?,
+                })?;
             }
         }
         None => {
@@ -58,23 +100,16 @@ pub fn run_command(
         }
     }
 
-    if !output.stderr.is_empty() && !expect_stderr {
-        return Err(CommandError::UnexpectedStderr {
-            cmd: command_string(cmd, args),
-            stderr: String::from_utf8(output.stderr)?,
-        })?;
+    Ok(output)
+}
+
+fn command_string(cmd: &String, args: &Vec<String>) -> String {
+    let mut cstr = cmd.clone();
+    if !args.is_empty() {
+        cstr.push(' ');
+        cstr.push_str(args.join(" ").as_str());
     }
-
-    let code = match output.status.code() {
-        Some(c) => c,
-        None => -1,
-    };
-
-    Ok(CommandResult {
-        exit_code: code,
-        stdout: to_option_string(output.stdout),
-        stderr: to_option_string(output.stderr),
-    })
+    cstr
 }
 
 fn to_option_string(v: Vec<u8>) -> Option<String> {
@@ -85,17 +120,9 @@ fn to_option_string(v: Vec<u8>) -> Option<String> {
     }
 }
 
-fn command_string(mut cmd: String, args: Vec<String>) -> String {
-    if !args.is_empty() {
-        cmd.push(' ');
-        cmd.push_str(args.join(" ").as_str());
-    }
-    cmd
-}
-
-fn signal_from_status(output: ExitStatus) -> i32 {
+fn signal_from_status(status: process::ExitStatus) -> i32 {
     #[cfg(target_family = "unix")]
-    match output.signal() {
+    match status.signal() {
         Some(s) => s,
         None => 0,
     }
