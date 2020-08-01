f5b6935d958edf73f49ba998f77b0f51aeecf9ac
Dave Rolsky <autarch@urth.org>
2020-08-01 13:38:21 -0500

    Move logging setup from main.rs to precious.rs

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,11 @@ mod path_matcher;
 mod precious;
 mod vcs;
 
-use anyhow::Result;
-use clap::ArgMatches;
-use fern::colors::{Color, ColoredLevelConfig};
-use fern::Dispatch;
 use log::error;
 
 fn main() {
     let matches = precious::app().get_matches();
-    let res = init_logger(&matches);
+    let res = precious::init_logger(&matches);
     if let Err(e) = res {
         eprintln!("Error creating logger: {}", e);
         std::process::exit(126);
@@ -34,41 +30,4 @@ fn main() {
         }
     };
     std::process::exit(status);
-}
-
-fn init_logger(matches: &ArgMatches) -> Result<(), log::SetLoggerError> {
-    let line_colors = ColoredLevelConfig::new()
-        .error(Color::Red)
-        .warn(Color::Yellow)
-        .info(Color::BrightBlack)
-        .debug(Color::BrightBlack)
-        .trace(Color::BrightBlack);
-
-    let level = if matches.is_present("trace") {
-        log::LevelFilter::Trace
-    } else if matches.is_present("debug") {
-        log::LevelFilter::Debug
-    } else if matches.is_present("verbose") {
-        log::LevelFilter::Info
-    } else {
-        log::LevelFilter::Warn
-    };
-
-    let level_colors = line_colors.clone().info(Color::Green).debug(Color::Black);
-    Dispatch::new()
-        .format(move |out, message, record| {
-            out.finish(format_args!(
-                "{color_line}[{target}][{level}{color_line}] {message}\x1B[0m",
-                color_line = format_args!(
-                    "\x1B[{}m",
-                    line_colors.get_color(&record.level()).to_fg_str()
-                ),
-                target = record.target(),
-                level = level_colors.color(record.level()),
-                message = message,
-            ));
-        })
-        .level(level)
-        .chain(std::io::stdout())
-        .apply()
 }
--- src/precious.rs
+++ src/precious.rs
@@ -5,6 +5,8 @@ use crate::filter;
 use crate::vcs;
 use anyhow::{Error, Result};
 use clap::{App, Arg, ArgGroup, ArgMatches, SubCommand};
+use fern::colors::{Color, ColoredLevelConfig};
+use fern::Dispatch;
 use log::{debug, error};
 use rayon::prelude::*;
 use std::collections::HashMap;
@@ -136,6 +138,44 @@ fn common_subcommand<'a>(name: &'a str, about: &'a str) -> App<'a, 'a> {
                 .args(&["all", "git", "staged", "paths"])
                 .required(true),
         )
+}
+
+pub fn init_logger(matches: &ArgMatches) -> Result<(), log::SetLoggerError> {
+    let line_colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::BrightBlack)
+        .debug(Color::BrightBlack)
+        .trace(Color::BrightBlack);
+
+    let level = if matches.is_present("trace") {
+        log::LevelFilter::Trace
+    } else if matches.is_present("debug") {
+        log::LevelFilter::Debug
+    } else if matches.is_present("verbose") {
+        log::LevelFilter::Info
+    } else {
+        log::LevelFilter::Warn
+    };
+
+    let level_colors = line_colors.clone().info(Color::Green).debug(Color::Black);
+
+    Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{target}][{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    line_colors.get_color(&record.level()).to_fg_str()
+                ),
+                target = record.target(),
+                level = level_colors.color(record.level()),
+                message = message,
+            ));
+        })
+        .level(level)
+        .chain(std::io::stderr())
+        .apply()
 }
 
 impl<'a> Precious<'a> {
