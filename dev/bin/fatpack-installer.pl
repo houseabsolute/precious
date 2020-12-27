8ce4bc30448e2bfee63e116bafa74ae5d5799508
Dave Rolsky <autarch@urth.org>
2020-12-27 14:47:48 -0600

    Add comment to fatpack-installer.pl explaining why it's needed

--- dev/bin/fatpack-installer.pl
+++ dev/bin/fatpack-installer.pl
@@ -3,6 +3,12 @@
 set -e
 set -x
 
+# By default fatpack includes anything that is loaded from site_perl. That
+# means if we have newer versions of core modules like Archive::Tar or
+# IO::Handle installed there, they get fatpacked. This scripts creates a clean
+# new perlbrew lib and installs just the needed prereqs for the script, then
+# fatpacks with that lib. This ensures that our fatpacked script only include
+# the minimum necessary modules.
 perlbrew lib create perl-5.30.1@fatpack-precious-downloader
 perlbrew exec --with perl-5.30.1@fatpack-precious-downloader 'cpanm -n App::FatPacker HTTP::Tinyish Path::Tiny'
 perlbrew exec --with perl-5.30.1@fatpack-precious-downloader 'fatpack file ./dev/bin/install-precious.pl' > ./dev/bin/install-precious.packed.pl
