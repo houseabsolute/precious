3c639331ea2598c0b515ad310ca732fcc4b3170d
Dave Rolsky <autarch@urth.org>
2021-01-30 11:51:42 -0600

    Add $HOME/bin to path in install-dev-tools.sh if needed
    The script will also tell you to add it to your $PATH permanently if it wasn't
    already there.

--- dev/bin/install-dev-tools.sh
+++ dev/bin/install-dev-tools.sh
@@ -19,6 +19,24 @@ if [ "$1" == "-v" ]; then
     set -x
 fi
 
+mkdir -p $HOME/bin
+
+set +e
+echo ":$PATH:" | grep --extended-regexp ":$HOME/bin:" >& /dev/null
+if [ "$?" -eq "0" ]; then
+    path_has_home_bin=1
+fi
+set -e
+
+if [ -z "$path_has_home_bin" ]; then
+    PATH=$HOME/bin:$PATH
+fi
+
 install_tools
+
+echo "Tools were installed into $HOME/bin."
+if [ -z "$path_has_home_bin" ]; then
+     echo "You should add $HOME/bin to your PATH."
+fi
 
 exit 0
