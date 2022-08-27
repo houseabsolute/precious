47e93c17c1771d88327f3e22872248f3952ac8d5
Dave Rolsky <autarch@urth.org>
2022-08-27 15:52:57 -0500

    Replace "Cli" -> "command line"

--- src/basepaths.rs
+++ src/basepaths.rs
@@ -21,7 +21,7 @@ pub enum Mode {
 impl fmt::Display for Mode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Mode::FromCli => write!(f, "paths passed on the Cli (recursively)"),
+            Mode::FromCli => write!(f, "paths passed on the command line (recursively)"),
             Mode::All => write!(f, "all files in the project"),
             Mode::GitModified => write!(f, "modified files according to git"),
             Mode::GitStaged => write!(f, "files staged for a git commit"),
