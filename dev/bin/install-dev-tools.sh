ae4db5d0f3be72d64510e1fe4dd3147118f360c8
Dave Rolsky <autarch@urth.org>
2023-03-04 11:38:51 -0600

    Install prettier locally and run it with --no-config

--- dev/bin/install-dev-tools.sh
+++ dev/bin/install-dev-tools.sh
@@ -14,7 +14,7 @@ function install_tools () {
     run "rustup component add clippy"
     run "ubi --project houseabsolute/precious --in ~/bin"
     run "ubi --project houseabsolute/omegasort --in ~/bin"
-    run "sudo npm install --global prettier"
+    run "npm install prettier"
 }
 
 if [ "$1" == "-v" ]; then
